e9275a9c1204e3cabeb99b7c70992c4b1794ac55
cassandra <meursault45@live.com>
2019-07-25 18:41:55 +0200

    clear button function

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-function returnResult () //rapport au clic de égal
-{
-    
-}
-
-
 let inputs = document.getElementsByTagName("button");
 let val = document.getElementsByTagName("p");
-let screen = document.getElementById("screen")
-let result = screen.textContent;
+let screen = document.getElementById("screen");
+let result = screen.textContent + this.textContent;
 
-function displayScreen ()    //Affichage de la valeur dans l'écran
+function displayScreen ()    //Affichage des valeurs dans l'écran
 {
     console.log(this.textContent);
     
-    if (this.textContent != "=", "CE", "←"){
-    screen.textContent = screen.textContent + this.textContent;
-}
-
-    if(this.textContent === "=")
+    if (this.textContent != "=", "CE", "←")
     {
-        eval(result);
+        screen.textContent = screen.textContent + this.textContent;
+    }
+
+    if (this.textContent === "=") //tofix
+    {
+        return eval(result);
+    }
+
+    if (this.textContent === "CE") //Effacement des valeurs dans l'écran
+    {
+        return screen.textContent = null;
+    }
+
+    if (this.textContent === "←") //tofix
+    {
+        return screen.textContent = screen.textContent;
     }
 }
     
